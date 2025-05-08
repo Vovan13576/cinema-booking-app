cb48ddc2fd6fb4a80f588fd1f90a6c3e6baf392c
Прокоф'єв Володимир bc2024-1 <vp3098874@gmail.com>
2025-05-08 16:28:12 +0300

    Додано маршрут для сторінки бронювання

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import Booking from './pages/Booking';
 import './App.css';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <div className="App">
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/booking/:id" element={<Booking />} />
         </Routes>
       </div>
     </Router>
