6a6887188f469d7c97a81ece8b3689b86c0a2e49
Прокоф'єв Володимир bc2024-1 <vp3098874@gmail.com>
2025-05-08 16:25:20 +0300

    Створено головну сторінку зі списком фільмів

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import MovieList from '../components/MovieList';
+import { movies } from '../data/movies';
+
+const Home = () => {
+  return (
+    <div className="home-page">
+      <h1>Афіша кінотеатру</h1>
+      <MovieList movies={movies} />
+    </div>
+  );
+};
+
+export default Home;
